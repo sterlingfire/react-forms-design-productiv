8ddc89f59845daa9354e0ec3cbfcb6c06a5292e0
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-21 14:32:24 -0800

    params

---
+++ productiv/src/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+/** Simple presentation component for a todo.
+ *
+ * Props:
+ * - todo: like { id, title, description, priority }
+ *
+ * { EditableTodo, TodoApp } -> Todo
+ **/
+
+function Todo({todo:{id, title, description, priority}}) {
+  return (
+      <div className="Todo">
+        <div><b>{title}</b> <small>(priority: {priority})</small></div>
+        <div><small>{description}</small></div>
+      </div>
+  );
+}
+
+export default Todo;
---
+++ productiv/src/TodoApp.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm";
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp({initalTodos}) {
+
+  const [todos, setTodos] = useState(initalTodos);
+
+  /** add a new todo to list */
+  function create(newTodo) {
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+  }
+
+  /** delete a todo by id */
+  function remove(id) {
+  }
+
+  return (
+      <main className="TodoApp">
+        <div className="row">
+
+          <div className="col-md-6">
+            <EditableTodoList todos={todos} update={update} remove={remove} /> OR
+            <span className="text-muted">You have no todos.</span>
+          </div>
+
+          <div className="col-md-6">
+            (if no top todo, omit this whole section)
+            <section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo todos={todos}/>
+            </section>
+
+            <section>
+              <h3 className="mb-3">Add Nü</h3>
+              <TodoForm />
+            </section>
+          </div>
+
+        </div>
+      </main>
+  );
+}
+
+export default TodoApp;
